3e5a0a0f231f389a247accdf53a8b80ecffc0b48
benjamindobbs <benjamindobbs@gmail.com>
2026-03-13 08:37:20 -0400

    cleanup

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -20,7 +20,6 @@ import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.KrakenX60;
 import frc.robot.Constants.KrakenX44;
 import frc.robot.Ports;
 
